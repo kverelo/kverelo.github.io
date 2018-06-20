ebb2001f6ba6a773d68293c188b67868be9b37a5
kverelo <40184705+kverelo@users.noreply.github.com>
2018-06-20 15:15:23 -0700

    ?

---
+++ assets/js/ripple.js
@@ -0,0 +1,12 @@
+﻿(function (window, $) {
+  
+  $(function() {
+    
+    
+    $('.ripple').on('click', function (event) {
+      event.preventDefault();
+      
+      var $div = $('<div/>'),
+          btnOffset = $(this).offset(),
+      		xPos = event.pageX - btnOffset.left,
+      		yPos = event.pageY - btnOffset.top;
